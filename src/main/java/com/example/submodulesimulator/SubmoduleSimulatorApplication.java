a4f2931221ecc93e3a7166fb4e2a9ba1f69db2ed
Mark Williams <mark.williams@currencycloud.com>
2023-10-05 10:32:08 +0100

    initial commit

---
+++ src/main/java/com/example/submodulesimulator/SubmoduleSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.submodulesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduleSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmoduleSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/submodulesimulator/SubmoduleSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submodulesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmoduleSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
